1b2f7122b68fc58f5a85ac7a5e5cb3ff3c08b2db
tania <Baletrot@gmail.com>
2021-05-17 11:58:05 +0200

    printerError function with map and reduce in one ine

--- main.js
+++ main.js
@@ -1,102 +1,43 @@
-// function createCalcFunction (n) {
-//   return function() {
-//       console.log(1000 * n)
-//   }
-// }
 
-// const calc = createCalcFunction(42);
-// calc();
+function printerError(s) {
+  
+  let numerator = s.split("").map((e, i) => s.charCodeAt(i) >= 110 ? "oops" : e ).reduce((acc, current) => {
 
+    if(current === "oops") {
+      return acc + 1;
+    } 
+    return acc;
+  }, 0);
 
+  let denominator = s.length;
+  let errorRate = `${numerator} / ${denominator}`;
 
-// function createIncrementor (n) {
-//   return function(num) {
-//     return n + num;
+  return errorRate;
+}
 
-//   }
-// }
-
-// const addOne = createIncrementor(1);
-// console.log(addOne(10));
-// const addTen = createIncrementor(10);
-// console.log(addTen(10));
-// console.log(addTen(41));
+console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
+console.log(printerError("avbnjxdtfrgsxyz"));
 
 
-// function urlGenerator(domain) {
-//   return function (url) {
-//     return `http://${url}.${domain}`
-//   }
-// }
 
-// const comUrl = urlGenerator('com');
-// console.log(comUrl("google"));
-// console.log(comUrl("twitter"));
-// console.log(comUrl("facebook"));
+// let str3 = "avbnjxdtfrgsxyz";
+// let newStr = str3.split("");
+// console.log(newStr);
+// let an = newStr.map((e, i) => str3.charCodeAt(i) >= 110 ? "oops" : e ).reduce((acc, current) => {
 
-// const ruUrl = urlGenerator('ru');
-// console.log(ruUrl('vkontakte'));
-// console.log(ruUrl('odnoklassniki'));
-// console.log(ruUrl('yandex'));
+//   if(current === "oops") {
+//     return acc + 1;
+//   } 
+//   return acc;
+// }, 0);
+// let result = newStr.reduce((acc, current) => )
+// console.log(an);
 
-// function bind(context, fn) {
-//   return function (...args) {
-//     fn.apply(context, args)
-    
-//   }
-// }
-// function logPerson() {
-//   console.log(`Person: ${this.name}, ${this.age}, ${this.job}`);
-// }
+// const indexOfLetters = [...Array(13).keys()].map((e, i) => i + 110);
+// let correctColors = indexOfLetters.map((x) => String.fromCharCode(x));
+// console.log(correctColors);
 
-// const person1 = { name: "Stas", age: 32, job: "developer" };
-// const person2 = { name: "Tania", age: 32, job: "inProgress" };
 
-// bind(person1, logPerson)();
-// bind(person2, logPerson)();
-
-// function makeFunction () {
-//   var name = "mozilla";
-//   function displayName() {
-//     alert (name);
-//   }
-//   return displayName;
-// } ;
-
-// var muFunc = makeFunction();
-// muFunc();
-
-// function makeAdder(x) {
-//   return function(y) {
-//     return x + y;
-//   };
-// };
-
-// var add5 = makeAdder(5);
-// var add10 = makeAdder(10);
-
-// console.log(add5(2));
-// console.log(add10(2));
-
-// let a = 15;
-
-// function hey() {
-//   let a = 10;
-// }
-
-// hey();
-// console.log(a);
-
-// var Counter = (function() {
-//   var privateCounter = 0;
-//   function changeBy(val) {
-//     privateCounter += val;
-//   }
-//   return {
-//     increment: function() {
-//       changeBy(1);
-//     },
-//     decrement: function() {
 
 
 
