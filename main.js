3613ee2e437f77866b9a7f4c08ba1f7f1fcc9023
tania <Baletrot@gmail.com>
2021-05-31 10:19:29 +0200

    learning about this keyword

--- main.js
+++ main.js
@@ -1,34 +1,73 @@
-const test = {
-  prop: 42,
-  func() {
-    return this.prop;
-  },
-};
+// const test = {
+//   prop: 42,
+//   func() {
+//     return this.prop;
+//   },
+// };
 
-console.log(test.func())
+// console.log(test.func())
 
-let a = 'Hello World!';
-function first() {
-  console.log('Inside first function');
-  second();
-  console.log('Again inside first function');
-}
-function second() {
-  console.log('Inside second function');
-}
-first();
-console.log('Inside Global Execution Context');
+// let a = 'Hello World!';
+// function first() {
+//   console.log('Inside first function');
+//   second();
+//   console.log('Again inside first function');
+// }
+// function second() {
+//   console.log('Inside second function');
+// }
+// first();
+// console.log('Inside Global Execution Context');
 
 let foo = {
-  baz: function() {
-  console.log(this);
+  name: "Vlad",
+  baz() {
+  return this;
   }
 }
-foo.baz();    // 'this' указывает на объект 'foo', так как функция 'baz' была вызвана 
+console.log(foo.baz());    // 'this' указывает на объект 'foo', так как функция 'baz' была вызвана 
              // как метод объекта 'foo'
 let bar = foo.baz;
-bar();       // 'this' указывает на глобальный объект window, так как при вызове функции
+console.log(bar());       // 'this' указывает на глобальный объект window, так как при вызове функции
              // ссылка на объект не используется
+
+const test = {
+  prop: 42,
+  func: function() {
+    return this;
+  },
+};
+   
+let ook = test.func;
+
+console.log(test.func());
+console.log(ook());
+
+function f1() {
+  return this;
+}
+
+console.log(f1());
+
+f1() === window;
+
+function f2(){
+  "use strict"; // см. strict mode
+  return this;
+}
+console.log(f2());
+
+var ob = {b: 'Custom'};
+var obj = {a: 'Custom'};
+var a = 'Global';
+function whatsThis() {
+  return this.a; 
+}
+
+console.log(whatsThis());
+console.log(whatsThis.call(obj));
+console.log(whatsThis.call(ob));
+
 
 
 
