37d26bb2e5c310ee75f82b4fa06ee8ee2491133b
tania <Baletrot@gmail.com>
2021-06-02 12:57:11 +0200

    event loop  basics

--- main.js
+++ main.js
@@ -91,96 +91,118 @@ const userWatchingCatMeme = false;
 //   console.log("Hey " + error.name + ' ' + error.message)
 // });
 //new function
-const recordViodeoOne = new Promise((resolve, reject) => {
-  resolve("Video 1 recorded");
+// const recordViodeoOne = new Promise((resolve, reject) => {
+//   resolve("Video 1 recorded");
+// });
+
+// const recordViodeoTwo = new Promise((resolve, reject) => {
+//   resolve("Video 2 recorded");
+// });
+
+// const recordViodeoThree = new Promise((resolve, reject) => {
+//   resolve("Video 3 recorded");
+// });
+
+// Promise.race([recordViodeoOne, recordViodeoTwo, recordViodeoThree]).then(
+//   (message) => {
+//     console.log(message);
+//   }
+// );
+
+
+// //new function
+// function divide(a, b) {
+//   return new Promise(function (resolve, reject) {
+//     if (b === 0) {
+//       reject(new Error("You can't divide by 0"));
+//       return;
+//     }
+
+//     resolve(a / b);
+//   });
+// }
+
+// console.log(divide(20, 0));
+
+// divide(10, 2)
+//   .then(function (result) {
+//     console.log(`Division Success: ${result}`);
+//   })
+//   .catch(function (error) {
+//     console.log("There was an error: " + error);
+//   });
+
+
+//   //new function
+// function wait(ms) {
+//   return new Promise(function (resolve, reject) {
+//     if (ms <= 0) {
+//       reject(new Error("Cannot wait for 0 seconds or less"));
+//       return;
+//     }
+
+//     setTimeout(function () {
+//       resolve("Hey");
+//     }, ms);
+//   });
+// }
+
+// wait(0)
+//   .then(function (message) {
+//     console.log("We have waited 1 sec " + message);
+//   })
+//   .catch(function (error) {
+//     console.log(`There was an error: ${error.message}`);
+//   });
+
+
+
+
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'chatroom'
+// });
+
+// connection.connect(function(err) {
+//   if(err) {
+//     console.log('There was an error:( ');
+//   }
+
+//   console.log('Connection successfull!');
+//   connection.end();
+// });
+
+// function foo() {
+//   throw new Error("OOps")
+// }
+//  function bar() {
+//     foo()
+//  }
+
+// function baz() {
+//   bar()
+// }
+
+// console.log(baz());
+
+[1,2,3,4].forEach(function (i) {
+  console.log(i)
 });
 
-const recordViodeoTwo = new Promise((resolve, reject) => {
-  resolve("Video 2 recorded");
-});
-
-const recordViodeoThree = new Promise((resolve, reject) => {
-  resolve("Video 3 recorded");
-});
-
-Promise.race([recordViodeoOne, recordViodeoTwo, recordViodeoThree]).then(
-  (message) => {
-    console.log(message);
-  }
-);
-
-
-//new function
-function divide(a, b) {
-  return new Promise(function (resolve, reject) {
-    if (b === 0) {
-      reject(new Error("You can't divide by 0"));
-      return;
-    }
-
-    resolve(a / b);
-  });
+function asyncArray(array, cb) {
+  array.forEach(function () {
+    setTimeout(cb, 0)
+  })
 }
 
-console.log(divide(20, 0));
 
-divide(10, 2)
-  .then(function (result) {
-    console.log(`Division Success: ${result}`);
-  })
-  .catch(function (error) {
-    console.log("There was an error: " + error);
-  });
-
-
-  //new function
-function wait(ms) {
-  return new Promise(function (resolve, reject) {
-    if (ms <= 0) {
-      reject(new Error("Cannot wait for 0 seconds or less"));
-      return;
-    }
-
-    setTimeout(function () {
-      resolve("Hey");
-    }, ms);
-  });
-}
-
-wait(0)
-  .then(function (message) {
-    console.log("We have waited 1 sec " + message);
-  })
-  .catch(function (error) {
-    console.log(`There was an error: ${error.message}`);
-  });
-
-
-
-
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'chatroom'
-});
-
-connection.connect(function(err) {
-  if(err) {
-    console.log('There was an error:( ');
-  }
-
-  console.log('Connection successfull!');
-  connection.end();
-});
-
-
-
-
-
-
+console.log(asyncArray([1,2,3,4], function(i) {
+  console.log(i)
+}));
 
 
 
