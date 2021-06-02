3c20986c205f5b2272bd46333e13bf6958f71cb6
tania <Baletrot@gmail.com>
2021-06-02 10:11:01 +0200

    promise example

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-let p = new Promise((resolve, reject) => {
-  let a = 1 + 1
-  if(a === 2) {
-    resolve('Success')
-  } else {
-    reject('Failed')
-  }
-})
+// let p = new Promise((resolve, reject) => {
+//   let a = 1 + 1
+//   if(a === 2) {
+//     resolve('Success')
+//   } else {
+//     reject('Failed')
+//   }
+// })
 
-p.then((message) => {
-  console.log('This is in the then ' + message)
-}).catch((message) => {
-  console.log('This is in the catch ' + message)
-})
+// p.then((message) => {
+//   console.log('This is in the then ' + message)
+// }).catch((message) => {
+//   console.log('This is in the catch ' + message)
+// })
 
 
 // doSomething()
@@ -26,19 +26,68 @@ p.then((message) => {
 // })
 // .catch(failureCallback);
 
-new Promise((resolve, reject) => {
-  console.log('Initial');
+// new Promise((resolve, reject) => {
+//   console.log('Initial');
 
-  resolve();
-})
-.then(() => {
-  throw new Error('Something failed');
+//   resolve();
+// })
+// .then(() => {
+//   throw new Error('Something failed');
 
-  console.log('Do this');
+//   console.log('Do this');
+// })
+// .catch(() => {
+//   console.error('Do that');
+// })
+// .then(() => {
+//   console.log('Do this, no matter what happened before');
+// });
+
+const userLeft = false;
+const userWatchingCatMeme = false;
+
+// function watchTutorialCallBack(callback, errorCallback) {
+//   if(userLeft) {
+//     errorCallback({
+//       name: 'User Left',
+//       message: ':('
+//     })
+//   } else if (userWatchingCatMeme) {
+//     errorCallback({
+//       name: 'User watchng cat meme',
+//       message:'WebDevSimplified < cat'
+//     })
+//   } else {
+//     callback('Thumbs up and subscribe')
+//   }
+// }
+
+// watchTutorialCallBack((message) => {
+//   console.log('Success: ' + message)
+// }, (error) => {
+//   console.log("Hey " + error.name + ' ' + error.message)
+// })
+
+function watchTutorialPromise() {
+return new Promise((resolve, reject) => {
+  if(userLeft) {
+    reject({
+      name: 'User Left',
+      message: ':('
+    })
+  } else if (userWatchingCatMeme) {
+    reject({
+      name: 'User watchng cat meme',
+      message:'WebDevSimplified < cat'
+    })
+  } else {
+    resolve('Thumbs up and subscribe')
+  }
 })
-.catch(() => {
-  console.error('Do that');
-})
-.then(() => {
-  console.log('Do this, no matter what happened before');
+}
+
+watchTutorialPromise().then((message) => {
+  console.log('Success: ' + message)
+}).catch((error) => {
+  console.log("Hey " + error.name + ' ' + error.message)
 });
