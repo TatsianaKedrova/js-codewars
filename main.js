3ae07e36cc38e037a85fb60ffa3011d8dcd53ce3
tania <Baletrot@gmail.com>
2021-06-02 09:39:59 +0200

    learning of promises

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
-function foo(b) {
-  let a = 10;
-  return a + b + 11
-}
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1
+  if(a === 2) {
+    resolve('Success')
+  } else {
+    reject('Failed')
+  }
+})
 
-function bar(x) {
-  let y = 3;
-  return foo(x * y)
-}
-
-console.log(bar(7));
-
+p.then((message) => {
+  console.log('This is in the then ' + message)
+}).catch((message) => {
+  console.log('This is in the catch ' + message)
+})
 
 
+// doSomething()
+// .then(function(result) {
+//   return doSomethingElse(result);
+// })
+// .then(function (newResult) {
+//   return doThirdThing(newResult);
+// })
+// .then(function (finalResult) {
+//   console.log('Got the final result: ' + finalResult)
+// })
+// .catch(failureCallback);
 
+new Promise((resolve, reject) => {
+  console.log('Initial');
 
+  resolve();
+})
+.then(() => {
+  throw new Error('Something failed');
 
+  console.log('Do this');
+})
+.catch(() => {
+  console.error('Do that');
+})
+.then(() => {
+  console.log('Do this, no matter what happened before');
+});
