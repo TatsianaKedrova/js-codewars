63c2987b0f3ffcc34ff53a3d387fe4d927af3412
tania <Baletrot@gmail.com>
2021-06-13 10:53:13 +0200

    context this

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
-const person = new Object({
+//This and context
+function hello() {
+  console.log("Hello", this);
+}
+
+const person = {
   name: "Tania",
   age: 32,
-  dance: function () {
-      console.log("She can dance very well");
-  }
-})
-
-console.log(person);
-
-const person1 = {
-  name: "Stas",
-  age: 32,
-  guitar: function () {
-      console.log("He can play guitar very well");
+  sayHello: hello,
+  sayHelloWindow: hello.bind(document),
+  logInfo: function() {
+    console.log(`Name is ${this.name}`)
   }
 }
-console.log(person1);
-
-Object.prototype.sayHi = function () {
-  console.log("Hello!");
-}
-
-const lena = Object.create(person);
-lena.name = "Elena";
-
-const str = "I am string";
-const stri = new String("I am phrase");
-
