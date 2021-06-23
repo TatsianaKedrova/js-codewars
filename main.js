25bff82c8210e8ea2b962ae2881fe5d5999407eb
TatsianaKedrova <Baletrot@gmail.com>
2021-06-23 12:51:26 +0200

    final result of task Find the divisors

--- main.js
+++ main.js
@@ -1,37 +1,17 @@
+//Find the divisors!
+
 function divisors(number) {
-  let isPrime = true;
   let arrOfDivisors = [];
 
   if (number <= 1) return;
 
-  // if (number > 1) {
-  //   for (let i = 2; i < number; i++) {
-  //     if (number % i === 0) {
-  //       isPrime = false;
-  //       break;
-  //     }
-  //   }
-    
+  if (number > 1) {
     for (let i = 2; i < number; i++) {
       if (number % i === 0) {
         arrOfDivisors.push(i);
       }
     }
-if(arrOfDivisors.length === 0 ) {
-  console.log(`${number} is prime`);
+  }
+    return arrOfDivisors.length ? arrOfDivisors : `${number} is prime`;
 }
-else {
-  return arrOfDivisors;
-}
-
-  //   console.log(arrOfDivisors);
-
-  //   if (isPrime) {
-  //     console.log(`${number} is prime`);
-  //   } else {
-  //     console.log(`${number} is not prime cos isPrime is ${isPrime}`);
-  //   }
-  // }
-}
-
-console.log(divisors(0));
+console.log(divisors(18));
