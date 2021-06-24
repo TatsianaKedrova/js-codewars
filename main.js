7e1e4436adac34341287e2bcb9c5e58d17ff90ae
TatsianaKedrova <Baletrot@gmail.com>
2021-06-24 11:49:31 +0200

    second varient of compare with margin task

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 // Compare within margin
 
-function closeCompare (a, b, margin) {
+// function closeCompare (a, b, margin) {
 
-  if(margin === undefined) margin = 0;
+//   if(margin === undefined) margin = 0;
 
- if( Math.abs(a - b) <= margin) {
-   return 0;
- } else if (a < b && Math.abs(a-b) > margin ) {
-   return -1;
- } else {
-   return 1;
- }
+//  if( Math.abs(a - b) <= margin) {
+//    return 0;
+//  } else if (a < b && Math.abs(a-b) > margin ) {
+//    return -1;
+//  } else {
+//    return 1;
+//  }
+
+// }
+
+
+function closeCompare (a, b, margin = 0) {
+
+  if(a < b - margin) return -1;
+  if(a - margin > b ) return 1;
+  return 0;
 
 }
 
-console.log(closeCompare(4, 5));
+console.log(closeCompare(10, 4, 7));
+
