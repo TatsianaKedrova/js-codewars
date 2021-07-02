93c36861fb3e05fbc20b11122d533207bf47e00d
TatsianaKedrova <Baletrot@gmail.com>
2021-07-02 08:22:05 +0200

    closures

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-// Bubbling
+// closure
 
+let a = 100;
+
+let say = () => {
+    a = 200;
+    
+}
+
+say();
+console.log(a);
+
+
+let name = "John";
+function sayHi() {
+    alert("Hi, " + name);
+}
+
+name = "Pete";
+
+sayHi();
