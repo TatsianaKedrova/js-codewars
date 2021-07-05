06903587ca94e1745c3c6a32f749df6b77de669c
TatsianaKedrova <Baletrot@gmail.com>
2021-07-05 14:12:38 +0200

    closures in functions

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-//this keyword
+// useCallback
 
-const module = {
-    x: 42,
-    getX() {
-        return this.x;
-    }
+function factory() {
+    return (a,b) => a + b;
 }
 
-console.log(module.getX());
-const unboundGetX = module.getX;
-const boundFetX = unboundGetX.bind(module);
+const sum1 = factory();
+const sum2 = factory();
 
-console.log(boundFetX());
+console.log(sum1(1,2));
+console.log(sum2(1,2));
+
