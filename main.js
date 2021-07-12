c59928f98ffbb7aba91accbc97908e73c2c6cf4a
TatsianaKedrova <Baletrot@gmail.com>
2021-07-12 11:38:40 +0200

    async/await function

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-// useCallback
+// async await
 
-function factory() {
-    return (a,b) => a + b;
+const waitAndGet = (waitingTime) => {
+    let randomNum = Math.random();
+    let resOrRej = Math.random();
+    resOrRej < 0.5 ? 
+    setTimeout( () => {
+        return Promise.resolve(randomNum);
+    }, waitingTime * 1000 )
+    : 
+    setTimeout(() => {
+        return new Error("This number is not acceptable fir the system");
+    })
+// return randomNum;
 }
 
-const sum1 = factory();
-const sum2 = factory();
+const start = async () => {
+    try {
+        const randNumber = await waitAndGet(3);
+        console.log(randNumber)
+    } catch {
+        console.log("number not generated");
+    }
 
-console.log(sum1(1,2));
-console.log(sum2(1,2));
+}
 
+console.log(5);
