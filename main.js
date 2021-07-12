8eb327fa3bcb7f96ae74a65dfc789cd48e784c15
TatsianaKedrova <Baletrot@gmail.com>
2021-07-12 13:19:28 +0200

    task finshed

--- main.js
+++ main.js
@@ -12,15 +12,76 @@ const waitAndGet = (waitingTime) => {
 }
 
 
-const start = async () => {
-    try {
-        const randNumber = await waitAndGet(3);
-        console.log(randNumber)
-    } catch {
-        console.log("number not generated");
-    }
+const start = async () => waitAndGet(3);
+// {
+//     try {
+//         const randNumber = await waitAndGet(3);
+//         console.log(randNumber)
+//     } catch {
+//         return Promise.reject();
+//     }
 
-}
+// }
 
 // console.log(start());
+
+// Promise.all([start(), start(), start()]).then((result)=> {
+//     console.log(result)
+// }).catch(err => console.log('one or more promises not generated',  err));
+
+
+
+// const hungry = true;
+
+// const eat = new Promise((res, rej) => {
+//     if(hungry) {
+//         const fastfood = {
+//             activity: 'cook noodles',
+//             location: 'McDonalds'
+//         };
+//         res(fastfood)
+//     } else {
+//         rej(err => new Error('Not hungry'))
+//     }
+// });
+
+// const willEat = function() {
+//     eat
+//         .then(result => {
+//         console.log('Going to eat noodles');
+//         console.log(hungry);
+//     })
+//         .catch(err => console.log(err.message))
+// }
+
+// console.log(willEat())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
