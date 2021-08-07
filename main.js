a27b3069ac67c90645eb9813e93c336a97b64544
TatsianaKedrova <Baletrot@gmail.com>
2021-08-07 09:07:03 +0200

    toString method for numbers

--- main.js
+++ main.js
@@ -1,130 +1,14 @@
-// async await
-
-
-
-const createUniqueNameGen = (uniqueName) => {
-let counter = 1;
-    
-    return () => {
-        let arr = uniqueName.split('');
-        if(arr.includes('.')) {
-            let indexBeforeDot = arr.indexOf('.');
-            arr.splice(indexBeforeDot, 0, counter)
-        }
-        counter++;
-        return arr.join('');
-    }
+// toString()
+function Dog(name) {
+    this.name = name;
 }
 
-const generator1 = createUniqueNameGen("song.mp3");
-const generator2 = createUniqueNameGen("tania-dance.com");
+const dog1 = new Dog('Gabby');
 
-console.log(generator1());
-console.log(generator1());
-console.log(generator1());
-console.log(generator1());
-console.log(generator2());
-console.log(generator1());
-console.log(generator1());
-console.log(generator1());
+Dog.prototype.toString = function dogToString() {
+    return `${this.name}`;
+};
 
-// const exampleFunc = (uniqueName) => {
-//     let counter = 1;
-//     return () => {
-//         let arr = uniqueName.split('.');
-//         let result = arr[0] + counter;
-//         counter++;
-//         return result;
-//     }   
-// }
+console.log(dog1.toString());
 
-// const func1 = exampleFunc("teddyBear.com");
-// console.log(func1());
-// console.log(func1());
-// console.log(func1());
-// console.log(func1());
-// console.log(func1());
-// console.log(func1());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const waitAndGet = (waitingTime) => {
-    
-//     return new Promise((res, rej) => {
-//         let randomNum = Math.random();
-//         let resOrRej = Math.random();
-//             setTimeout(() => {
-//                resOrRej <= 0.5 ? res(randomNum) :  rej()     
-//             }, waitingTime * 1000)
-//     })
-// }
-
-
-// const start = async () => waitAndGet(3);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log((10).toString());
