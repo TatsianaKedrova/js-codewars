2991bbac3fda03211a08579584dbe553cff9e195
TatsianaKedrova <Baletrot@gmail.com>
2021-08-11 07:51:38 +0200

    repeat sort method

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-//spread operator
+//sorting
 
-function sum(x, y, z) {
-    return x + y + z;
+const months = ['March', undefined, 'Jan', undefined, 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [undefined, 1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// expected output: Array [1, 100000, 21, 30, 4]
+
+const arr = [56,7,42,31,28,96,4,15,27]
+
+arr.sort();
+console.log(arr);
+
+arr.sort((a, b) => a - b);
+console.log(arr);
+
+let items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 }
+];
+
+items.sort((a, b) => {
+  
+  let aValue = a.name.toUpperCase();
+  let bValue = b.name.toUpperCase();
+
+  if(aValue < bValue) {
+    return -1;
+  } else if(aValue > bValue) {
+    return 1; 
+  } else {
+    return 0;
   }
+});
+console.log(items);
 
-  const numbers = [1, 2, 3];
-
-  console.log(sum(...numbers));
-  console.log(sum.apply(null, numbers));
-
-  let numberStore = [0, 1, 2];
-let newNumber = 12;
-numberStore = [...numberStore, newNumber];
-
-console.log(numberStore);
-console.log(numberStore);
-console.log(numberStore);
-console.log(numberStore);
 
 
