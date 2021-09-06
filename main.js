d4a6368239504cbd732fa09fc4acf118131a2935
TatsianaKedrova <Baletrot@gmail.com>
2021-09-06 12:34:06 +0200

    var and function: priority of execution

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-//write oen map function
+//priority of execution
 
-let array1 = [1,2,3,4,5];
-function mapOwn(arr, mapFunc) {
-    const mapArr = [];
-    for(let i = 0; i < arr.length; i++) {
-        const result = mapFunc(arr[i], i, arr);
-        mapArr.push(result);
-    }
-    return mapArr;
+console.log(a);
+var a = 10;
+
+console.log(a);
+
+function a() {
+    console.log('vlad');
 }
 
-const la = mapOwn(array1, x => x * 3);
-console.log(la);
-
-
-function vladka() {
-    a = 3;
-}
-
-if(true) { var a = 2}
-vladka();
-
-console.log("value", a);
-
-//var, let, const
-
-// console.log(c);
-let c = 10;
-
-
-var d;
-console.log(d);
-d =10;
-console.log(d);
-console.log(e);
-var e = 2000;
-
-
+console.log(a);
