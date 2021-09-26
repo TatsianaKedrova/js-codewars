a429dac38bfb0a708db544f6e58ab4007650a2c8
TatsianaKedrova <Baletrot@gmail.com>
2021-09-26 20:44:08 +0200

    objects

--- main.js
+++ main.js
@@ -1,37 +1,10 @@
-//function
+//Object.entries
 
-function foo(a) {
-    var b = a + 2;
+const obj = {
+    a: "Tania",
+    b: 33
+};
 
-    function bar(c) {
-        console.log(a,b,c)
-    }
-
-    bar(b * 2);
-}
-
-foo(3);
-
-function foo2() {
-    var a = 2;
-
-    function bar() {
-        console.log(a);
-    }
-    return bar;
-}
-
-var baz = foo2();
-
-baz();
-console.log(foo2());
-
-function fib1(n) {
-
-    if (n <=1) {
-        return 1
-    }
-
-    return fib1(n - 1) + fib1(n - 2)
-
+for(const [key, value] of Object.entries(obj)) {
+    console.log(`${key}: ${value}`)
 }
