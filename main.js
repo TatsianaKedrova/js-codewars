589b491d1e14ad04499eb979b2cae1ff85d833cc
TatsianaKedrova <Baletrot@gmail.com>
2021-10-27 11:01:48 +0200

    regex repetition

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-//Object.entries
+//regex
 
-const obj = {
-    a: "Tania",
-    b: 33
-};
+const value = '1277775555';
+const value2 = '295312794';
+// const newVal = value.split('');
+// const spliceVal = newVal.splice(4,0,'-').join('');
 
-for(const [key, value] of Object.entries(obj)) {
-    console.log(`${key}: ${value}`)
-}
+// console.log(newVal);
+// console.log(newVal.join(''));
+
+
+console.log(value.replace(/(\d{4})(\d{4})(\d{2})/, "$1-$2-$3"));
+console.log(value2.replace(/(\d{4})(\d{4})(\d{1})/, "$1-$2-$3"));
+console.log(value2.replace(/[\d]{4}/g, '$&-'));
+
+console.log( "Люблю HTML".replace(/HTML/, "$& и JavaScript"));
