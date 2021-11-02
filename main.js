f4d60c7829243d8f8433e5a3b3d3e6087cfe16d8
TatsianaKedrova <Baletrot@gmail.com>
2021-11-02 09:32:23 +0200

    slice javascript

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-//regex
+//strings javascript
 
-const value = '1277775555';
-const value2 = '295312794';
-// const newVal = value.split('');
-// const spliceVal = newVal.splice(4,0,'-').join('');
-
-// console.log(newVal);
-// console.log(newVal.join(''));
-
-
-console.log(value.replace(/(\d{4})(\d{4})(\d{2})/, "$1-$2-$3"));
-console.log(value2.replace(/(\d{4})(\d{4})(\d{1})/, "$1-$2-$3"));
-console.log(value2.replace(/[\d]{4}/g, '$&-'));
-
-console.log( "Люблю HTML".replace(/HTML/, "$& и JavaScript"));
+let str = 'Vasiliy Lanovoi';
+console.log(str.includes(" "));
+console.log(str.indexOf(" "));
+if(str.includes(' ')) {
+    console.log( `${str.slice(0,1)}${str.slice(str.indexOf(' ')+1, str.indexOf(' ')+2)}`);
+};
