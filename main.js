a78e854cf2183bff3a86fb7bb269b7dd34907d00
TatsianaKedrova <Baletrot@gmail.com>
2022-02-03 12:22:33 +0200

    factory design pattern

--- main.js
+++ main.js
@@ -1,47 +1,41 @@
-// function getName () {
-//   return this.name
-// }
-
-// getName();
-// console.log("hello");
-
-// let sally = {
-//   name: "Sally"
-// }
-
-// let tania = {
-//   name: "Tania"
-// }
-
-// console.log(tania);
-// console.log(tania.age);
-
-// console.log(getName.bind(sally)());
-// console.log(getName.bind(tania)());
-
-// let getSurname = () => {
-//   return this.name
-// }
-
-// console.log(getSurname.bind(tania)());
-
-const divHeight = document.querySelector(".divHeight");
-
-const a = divHeight.getAttribute('style');
-const b = divHeight.scrollHeight;
-const c = divHeight.clientHeight;
-console.log("soething: ", a);
-console.log("scrollheight: ", b);
-console.log("clientHeight: ", c);
-
-const d = divHeight.getBoundingClientRect();
-console.log(d);
-
-try {
-  await fetch('http://example.com');
-}
-catch (err){
-  console.log("err: ", err);
+function Developer(name) {
+  this.name = name;
+  this.type = "Developer";
 }
 
+function Tester(name) {
+  this.name = name;
+  this.type = "Tester";
+}
 
+function EmployeeFactory() {
+  this.create = (name, type) => {
+    switch (type) {
+      case 1:
+        return new Developer(name);
+        break;
+      case 2:
+        return new Tester(name);
+        break;
+    }
+  };
+}
+
+function sayHello() {
+  console.log("Hello " + this.name + ", you are a " + this.type);
+}
+
+const employeeFactory = new EmployeeFactory();
+const employees = [];
+employees.push(employeeFactory.create("Tania", 1));
+employees.push(employeeFactory.create("Stas", 2));
+employees.push(employeeFactory.create("Elena", 2));
+employees.push(employeeFactory.create("Jenia", 1));
+employees.push(employeeFactory.create("Liosha", 1));
+employees.push(employeeFactory.create("Olesya", 2));
+
+console.log(employees);
+employees.forEach(emp => {
+  sayHello.call(emp)
+})
+// console.log();
