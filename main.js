860833684ca78e76dccad3486898b962915b4837
TatsianaKedrova <Baletrot@gmail.com>
2022-03-22 10:06:36 +0200

    feat(vers-0.1): deleted unnecessary packages including node_modules

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
-function Developer(name) {
-  this.name = name;
-  this.type = "Developer";
+function *generate() {
+  console.log("first step");
+  yield;
+  console.log(" second step");
 }
-
-function Tester(name) {
-  this.name = name;
-  this.type = "Tester";
-}
-
-function EmployeeFactory() {
-  this.create = (name, type) => {
-    switch (type) {
-      case 1:
-        return new Developer(name);
-        break;
-      case 2:
-        return new Tester(name);
-        break;
-    }
-  };
-}
-
-function sayHello() {
-  console.log("Hello " + this.name + ", you are a " + this.type);
-}
-
-const employeeFactory = new EmployeeFactory();
-const employees = [];
-employees.push(employeeFactory.create("Tania", 1));
-employees.push(employeeFactory.create("Stas", 2));
-employees.push(employeeFactory.create("Elena", 2));
-employees.push(employeeFactory.create("Jenia", 1));
-employees.push(employeeFactory.create("Liosha", 1));
-employees.push(employeeFactory.create("Olesya", 2));
-
-console.log(employees);
-employees.forEach(emp => {
-  sayHello.call(emp)
-})
-// console.log();
