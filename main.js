f2c652513a076fcf92ec747fb3fa9515a15d0c16
TatsianaKedrova <Baletrot@gmail.com>
2022-04-22 16:40:47 +0200

    linearSearch algorythm

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+const array = [1, 7, 4, 6, 9, 3, 2, 45, 78, 3, 6, 1];
+let count = 0;
 
+function linearSearch(array, item) {
+  let searchParam;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      searchParam = array[i];
+    }
+    console.log("searchParam: ", searchParam);
+  }
+  return searchParam;
+}
+
+console.log(linearSearch(array, 3));
+
+function linearSearch2(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        count += 1
+      if (array[i] === item) {
+        return i;
+      }
+     
+    }
+    return null;
+  }
+
+console.log(linearSearch2(array, 9));
+console.log("count: ", count);
+  
