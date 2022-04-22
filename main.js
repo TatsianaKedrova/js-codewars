178f72baf688ca2f1a2d4baeebe535bf0a73ba05
TatsianaKedrova <Baletrot@gmail.com>
2022-04-22 17:01:38 +0200

    binarySearch algorythm

--- main.js
+++ main.js
@@ -12,19 +12,45 @@ function linearSearch(array, item) {
   return searchParam;
 }
 
-console.log(linearSearch(array, 3));
+// console.log(linearSearch(array, 3));
 
 function linearSearch2(array, item) {
-    for (let i = 0; i < array.length; i++) {
-        count += 1
-      if (array[i] === item) {
-        return i;
-      }
-     
+  for (let i = 0; i < array.length; i++) {
+    count += 1;
+    if (array[i] === item) {
+      return i;
     }
-    return null;
   }
+  return null;
+}
 
-console.log(linearSearch2(array, 9));
+// console.log(linearSearch2(array, 9));
+// console.log("count: ", count);
+
+function binarySearch(array, item) {
+  let start = 0;
+  let end = array.length - 1;
+  let middle;
+  let found = false;
+  let position = -1;
+
+  while (found === false && start <= end) {
+    count += 1;
+    middle = Math.floor((start + end) / 2);
+    if (array[middle] === item) {
+      found = true;
+      position = middle;
+      return position;
+    }
+    if (item < array[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return position;
+}
+
+console.log(binarySearch(array, 78));
 console.log("count: ", count);
-  
+
