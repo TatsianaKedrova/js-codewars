9011694de1e9847946a238e4ae9f0a833d730579
TatsianaKedrova <Baletrot@gmail.com>
2023-03-29 10:11:05 +0200

    split to words solution

--- main.js
+++ main.js
@@ -1,56 +1,71 @@
-const array = [1, 7, 4, 6, 9, 3, 2, 45, 78, 3, 6, 1];
-let count = 0;
+//task 1
+// function expression
+// var boo = function tania() {
+//   return 2;
+// };
+// console.log(typeof tania());
 
-function linearSearch(array, item) {
-  let searchParam;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === item) {
-      searchParam = array[i];
-    }
-    console.log("searchParam: ", searchParam);
+var invokedFunc = (function () {
+  console.log("Code runs!");
+})();
+
+!(function () {
+  console.log("Code runs twice!");
+})();
+
+//task 2
+function* stas(num) {
+  for (let i = 0; i < num; i++) {
+    yield i;
   }
-  return searchParam;
 }
 
-// console.log(linearSearch(array, 3));
+var func2 = stas(5);
+console.log(func2.next());
+console.log(func2.next());
 
-function linearSearch2(array, item) {
-  for (let i = 0; i < array.length; i++) {
-    count += 1;
-    if (array[i] === item) {
-      return i;
-    }
-  }
-  return null;
-}
+// let time = {
+//   hour: 2,
+//   milli: this.hour,
+//   seconds: () => 3600 * this.hour,
+//   minutes() {
+//     return 60 * this.hour;
+//   },
+// };
 
-// console.log(linearSearch2(array, 9));
-// console.log("count: ", count);
+// console.log(time.minutes());
+// console.log(time.seconds());
+// console.log(time.milli);
 
-function binarySearch(array, item) {
-  let start = 0;
-  let end = array.length - 1;
-  let middle;
-  let found = false;
-  let position = -1;
+// const module = {
+//   x: 42,
+//   getX: function () {
+//     return this.x;
+//   },
+// };
 
-  while (found === false && start <= end) {
-    count += 1;
-    middle = Math.floor((start + end) / 2);
-    if (array[middle] === item) {
-      found = true;
-      position = middle;
-      return position;
-    }
-    if (item < array[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
-  }
-  return position;
-}
+// const unbound = module.getX();
+// console.log(unbound);
 
-console.log(binarySearch(array, 78));
-console.log("count: ", count);
+//task 2
+// console.log(notHoisted);
+// var notHoisted = function() {
+//   console.log('bar');
+// };
 
+// var first = 12345;
+
+// var func1 = function () {
+//   console.log(first);
+// };
+// func1();
+
+//task promises
+const obj = {
+  name: "tania",
+  age: "34",
+  job: "Software Developer",
+};
+
+const bbb = [obj, { height: "58" }];
+console.log("bbb:", bbb);
---
+++ test/test.js
@@ -0,0 +1,22 @@
+function splitToWords(sentence) {
+  const arrayOfWords = sentence
+    .replace(/-/g, "")
+    .replace(/\s+/g, " ")
+    .trim()
+    .split(" ")
+    .map((element) => element.toLowerCase());
+  return arrayOfWords;
+}
+
+function hasWhiteSpace(sentence) {
+  return /\s/g.test(sentence);
+}
+
+let phrase =
+  "                  JS  -     is    a      programming      language";
+console.log(splitToWords(phrase));
+console.log(hasWhiteSpace(phrase));
+
+const str = "zero?one.two_three-four";
+const newStr = str.replace(/[-._?]/g, "");
+console.log("string: ", newStr);
